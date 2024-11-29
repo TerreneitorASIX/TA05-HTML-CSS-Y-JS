f14081c53ed349615832196d1577fe01e1625f72
BryanAguilera-ITB2425 <bryan.aguilera.7e6@itb.cat>
2024-11-29 09:16:59 +0100

    Add files via upload

---
+++ Js/alerta.js
@@ -0,0 +1,8 @@
+// Seleccionar el elemento con el id "telefono-texto"
+const telefonoTexto = document.getElementById('alerta');
+
+// Agregar un evento de clic al elemento
+telefonoTexto.addEventListener('click', () => {
+    // Mostrar la alerta con el número de teléfono
+    alert('El número telefónico de Bryan es: +34 123 456 890');
+});
