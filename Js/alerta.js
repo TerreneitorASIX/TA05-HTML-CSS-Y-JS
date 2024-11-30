e7fe68610da003eca2062447b2a9d6feebb9cdec
BryanAguilera-ITB2425 <bryan.aguilera.7e6@itb.cat>
2024-11-30 14:14:57 +0100

    Delete Js directory

--- Js/alerta.js
+++
@@ -1,8 +0,0 @@
-// Seleccionar el elemento con el id "telefono-texto"
-const telefonoTexto = document.getElementById('alerta');
-
-// Agregar un evento de clic al elemento
-telefonoTexto.addEventListener('click', () => {
-    // Mostrar la alerta con el número de teléfono
-    alert('El número telefónico de Bryan es: +34 123 456 890');
-});
